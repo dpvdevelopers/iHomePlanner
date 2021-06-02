c5c9484e33d4ad4be2e9124f3f7f064d4253981b
Daniel <dpvdevelopers@gmail.com>
2021-06-02 10:56:23 +0200

    Modificada clase Group

--- app/src/main/java/com/example/ihomeplanner/Classes/Group.java
+++ app/src/main/java/com/example/ihomeplanner/Classes/Group.java
@@ -1,5 +1,11 @@
 package com.example.ihomeplanner.Classes;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Objects;
+
 public class Group {
     private double id;
     private String name;
@@ -33,5 +39,63 @@ public class Group {
         this.name = name;
         this.totalUsers = 0;
         this.license = license;
+    }
+    /*
+                            FIN CONSTRUCTORES
+     */
+
+    public double getId() {
+        return id;
+    }
+
+    public void setId(double id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalUsers() {
+        return totalUsers;
+    }
+
+    public void setTotalUsers(int totalUsers) {
+        this.totalUsers = totalUsers;
+    }
+
+    public int getLicense() {
+        return license;
+    }
+
+    public void setLicense(int license) {
+        this.license = license;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Group)) return false;
+        Group group = (Group) o;
+        return Double.compare(group.id, id) == 0;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "name='" + name + '\'' +
+                ", totalUsers=" + totalUsers +
+                ", license=" + license +
+                '}';
     }
 }
