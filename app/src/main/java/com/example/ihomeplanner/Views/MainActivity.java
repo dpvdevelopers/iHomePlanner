9671a95e1253fd6f666d93e47e4d0b777694d199
Daniel <dpvdevelopers@gmail.com>
2021-06-04 18:11:11 +0200

    Modificada vista activity_main.xml

---
+++ app/src/main/java/com/example/ihomeplanner/Views/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ihomeplanner.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.ihomeplanner.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Entrar(View view) {
+    }
+
+    public void Recuperar(View view) {
+    }
+}
