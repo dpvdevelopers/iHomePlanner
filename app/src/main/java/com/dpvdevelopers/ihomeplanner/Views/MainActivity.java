38cab0522c1ada62bc3fd11bb6262b7fd4317180
Daniel <dpvdevelopers@gmail.com>
2021-06-05 19:39:01 +0200

    añadidos métodos necesarios para la autenticación con Firebase

--- app/src/main/java/com/dpvdevelopers/ihomeplanner/Views/MainActivity.java
+++ app/src/main/java/com/dpvdevelopers/ihomeplanner/Views/MainActivity.java
@@ -1,19 +1,58 @@
 package com.dpvdevelopers.ihomeplanner.Views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.dpvdevelopers.ihomeplanner.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText edtUser;
+    private EditText edtPass;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUser = findViewById(R.id.edtUsuario);
+        edtPass = findViewById(R.id.edtPass);
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user != null){
+                    //Aquí ha iniciado sesión
+                }else{
+                    //El usuario salió de la sesión
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener != null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 
     public void Entrar(View view) {
